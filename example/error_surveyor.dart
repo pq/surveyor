f31e897b223e961053a4394dfa80cf7ffb830ce8
pq <pquitslund@gmail.com>
2019-06-18 17:00:39 -0700

    sync reporting

--- example/error_surveyor.dart
+++ example/error_surveyor.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 
 import 'package:analyzer/dart/analysis/analysis_context.dart';
+
+import 'package:analyzer/src/generated/engine.dart'
+    show AnalysisEngine, AnalysisErrorInfo, AnalysisErrorInfoImpl;
+
+import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/error/error.dart';
+import 'package:surveyor/src/analysis.dart';
 import 'package:surveyor/src/driver.dart';
 import 'package:surveyor/src/visitors.dart';
 
@@ -50,8 +56,20 @@ main(List<String> args) async {
 int dirCount;
 
 class AnalysisAdvisor extends SimpleAstVisitor
-    implements PreAnalysisCallback, PostAnalysisCallback, ErrorFilter {
+    implements
+        PreAnalysisCallback,
+        PostAnalysisCallback,
+        PostVisitCallback,
+        ErrorReporter {
   int count = 0;
+
+  AnalysisStats stats;
+  HumanErrorFormatter formatter;
+
+  AnalysisAdvisor() {
+    stats = new AnalysisStats();
+    formatter = HumanErrorFormatter(stdout, stats);
+  }
 
   @override
   void postAnalysis(AnalysisContext context, DriverCommands cmd) {
@@ -73,7 +91,6 @@ class AnalysisAdvisor extends SimpleAstVisitor
     print("Analyzing '$dirName' • [${++count}/$dirCount]...");
   }
 
-  @override
   bool showError(AnalysisError error) {
     final errorType = error.errorCode.type;
     if (errorType == ErrorType.HINT ||
@@ -85,8 +102,24 @@ class AnalysisAdvisor extends SimpleAstVisitor
     //print('${error.errorCode.type} :  ${error.errorCode.name}');
     return true;
   }
+
+  @override
+  void reportError(AnalysisResultWithErrors result) {
+    final errors = result.errors.where(showError).toList();
+    if (errors.isEmpty) {
+      return;
+    }
+    formatter
+        .formatErrors([new AnalysisErrorInfoImpl(errors, result.lineInfo)]);
+    formatter.flush();
+  }
+
+  @override
+  void onVisitFinished() {
+    stats.print();
+  }
 }
 
 //
 /// If non-null, stops once limit is reached (for debugging).
-int _debuglimit; // = 30;
+int _debuglimit; // = 300;
